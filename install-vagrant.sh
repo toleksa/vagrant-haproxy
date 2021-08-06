0eacae82fc18a6a7e25b2322a22f53e74be2e549
root <root@vagrant.ac>
2021-08-06 07:41:51 +0200

    final version

--- install-vagrant.sh
+++ install-vagrant.sh
@@ -4,12 +4,14 @@ set -e
 
 yum -y update
 yum install -y libvirt libvirt-devel ruby-devel gcc qemu-kvm libguestfs-tools make rsync
+systemctl start libvirtd
+systemctl enable libvirtd
 yum install -y yum-utils
 yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
 yum -y install vagrant
 
 #bugs
-yum install -y wget byacc cmake gcc-c++
+yum install -y wget byacc cmake gcc-c++ zlib-devel
 OLDDIR=`pwd`
 cd /tmp
 
@@ -22,7 +24,6 @@ cd /tmp
     cmake ../libssh-0.9.4 -DOPENSSL_ROOT_DIR=/opt/vagrant/embedded/
     make
     cp lib/libssh* /opt/vagrant/embedded/lib64
-    #ln -sf /opt/vagrant/embedded/lib64/libssl.so.4 /usr/lib64/libssh.so.4
 
     #https://github.com/vagrant-libvirt/vagrant-libvirt/issues/1127
     dnf download --source krb5-libs
@@ -32,12 +33,10 @@ cd /tmp
     LDFLAGS='-L/opt/vagrant/embedded/' ./configure
     make
     cp -a lib/crypto/libk5crypto.* /opt/vagrant/embedded/lib64/
-    #ln -sf /opt/vagrant/embedded/lib64/libk5crypto.so.3 /usr/lib64/libk5crypto.so.3
 
-cd $OLDDIR ; unset $OLDPWD
+cd $OLDDIR ; unset OLDPWD
 #end bugs
 
-export CONFIGURE_ARGS="with-libvirt-include=/usr/include/libvirt with-libvirt-lib=/usr/lib64"
 vagrant plugin install vagrant-libvirt
 
 vagrant plugin list
